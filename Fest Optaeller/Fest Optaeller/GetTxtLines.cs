dd9d3e20093ff50454df181da36cc350928022b0
ThiccBoi155 <danielfich2@gmail.com>
2021-10-16 12:07:15 +0200

    Moved project over

---
+++ Fest Optaeller/Fest Optaeller/GetTxtLines.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace Fest_Optaeller
+{
+    static class GetTxtLines
+    {
+        static string txtName = "people.txt";
+
+        // Returns all lines of the file named txtName that is on the same directory as the .exe file
+        // //Returns null if the file doesn't exist
+        // Throws exception if the file doesn't exist
+        public static string[] ReadTxtLines()
+        {
+            string txtFilePath = string.Format("{0}/{1}", new DirectoryInfo(".").FullName, txtName);
+
+            try
+            {
+                return File.ReadAllLines(txtFilePath);
+            }
+            catch (FileNotFoundException e)
+            {
+                throw new ArgumentException(string.Format("File named: {0} needs to exist", txtName));
+                //return null;
+            }
+        }
+    }
+}
---
+++ Fest Optaeller/Fest Optaeller/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fest_Optaeller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LoopLines();
+
+            PrintResults();
+
+            Console.ReadKey();
+        }
+
+        static int groupPeople = 0;
+        static int groupGirls = 0;
+
+        static int totalPeople = 0;
+        static int totalGirls = 0; // -
+
+        static int beerEnjoyers = 0; // ! e
+        static int neutralBeer = 0;  // # n
+        static int beerRefusers = 0; // ¤ r
+
+        static void LoopLines()
+        {
+            string[] lines = GetTxtLines.ReadTxtLines();
+
+            bool stopCount = false;
+
+            foreach (string line in lines)
+            {
+                if (line != "" && line[0] == '.')
+                    stopCount = !stopCount;
+
+                else if (line != "" && line[0] == '/')
+                    ;
+                else if (!stopCount && line != "")
+                {
+                    CountLine(line);
+                }
+                else if (!stopCount && groupPeople > 0)
+                {
+                    PrintGroupStats();
+                    ResetGroupStats();
+                }
+
+                Console.WriteLine(line);
+            }
+        }
+
+        static void CountLine(string line)
+        {
+            bool leaveLoop = false;
+            while (line.Length > 0 && !leaveLoop)
+            {
+                switch (line[0])
+                {
+                    case ' ':
+                        line = line.Substring(1);
+                        break;
+
+                    case '-':
+                        groupGirls++;
+                        line = line.Substring(1);
+                        break;
+
+                    case '!':
+                        beerEnjoyers++;
+                        line = line.Substring(1);
+                        break;
+                    case '#':
+                        neutralBeer++;
+                        line = line.Substring(1);
+                        break;
+                    case '¤':
+                        beerRefusers++;
+                        line = line.Substring(1);
+                        break;
+
+                    default:
+                        leaveLoop = true;
+                        break;
+                }
+            }
+            groupPeople++;
+        }
+
+        static void PrintGroupStats()
+        {
+            Console.WriteLine($"{groupPeople} -{groupGirls}");
+        }
+
+        static void ResetGroupStats()
+        {
+            totalPeople += groupPeople;
+            totalGirls += groupGirls;
+
+            groupPeople = 0;
+            groupGirls = 0;
+        }
+
+        static void PrintResults()
+        {
+            string f = string.Format("{0:0.00}", 100f * totalGirls / totalPeople);
+
+            Console.WriteLine($"{totalPeople} -{totalGirls} {f}%");
+            Console.WriteLine($"Beer enjoyers: {beerEnjoyers}");
+            Console.WriteLine($"Neutral beer: {neutralBeer}");
+            Console.WriteLine($"Beer refusers: {beerRefusers}");
+        }
+    }
+}
